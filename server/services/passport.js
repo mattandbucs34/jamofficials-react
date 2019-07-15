ba5cb121ad80a247a8a762d884f2b2372a4a0978
mattandbucs34 <35737262+mattandbucs34@users.noreply.github.com>
2019-07-15 15:52:24 -0500

    add login query if user exists

--- server/services/passport.js
+++ server/services/passport.js
@@ -12,6 +12,13 @@ passport.use(new GoogleStrategy(
     callbackURL: '/auth/google/callback'
   },
   (accessToken, refreshToken, profile, done) => {
-    new User({ googleId: profile.id }).save();
+    User.findOne({ googleId: profile.id }).then(existingUser => {
+      if(existingUser) {
+        //Profile ID already exists
+      }else {
+        //Profile ID not found, create new User Record
+        new User({ googleId: profile.id }).save();
+      }
+    });
   }
 ));
