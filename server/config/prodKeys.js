a46fe82107423e83d390e6b14cef77f1cae93189
mattandbucs34 <35737262+mattandbucs34@users.noreply.github.com>
2019-07-15 17:14:31 -0500

    updated environment keys

--- server/config/prodKeys.js
+++ server/config/prodKeys.js
@@ -1,7 +1,6 @@
-//dev.js - DO NOT COMMIT
 module.exports = {
-  googleClientID: '614339935715-leq7uh1egkf165gbqpml85dca930mf5d.apps.googleusercontent.com',
-  googleClientSecret: 'xGrzW5i8kpAwFOgVG5V3r0C0',
-  mongoURI: 'mongodb+srv://jamassign1:YgTPnBR3V0EDYBck@cluster0-oerto.mongodb.net/test?retryWrites=true&w=majority',
-  cookieKey: 'UxEm!{L2~okZbeM+D*O-NVPB+pyKM-'
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
 }
