56a7231311f83df9c7aac01db01a072ab47d108b
mattandbucs34 <35737262+mattandbucs34@users.noreply.github.com>
2019-07-16 12:41:39 -0500

    added React App and refactored promise in passport.js to async/await

--- server/services/passport.js
+++ server/services/passport.js
@@ -23,17 +23,15 @@ passport.use(new GoogleStrategy(
     callbackURL: '/auth/google/callback',
     proxy: true
   },
-  (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id }).then(existingUser => {
-      if(existingUser) {
-        //Profile ID already exists
-        done(null, existingUser);
-      }else {
-        //Profile ID not found, create new User Record
-        new User({ googleId: profile.id })
-          .save()
-          .then(user => done(null, user));
-      }
-    });
+  async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({ googleId: profile.id });
+    if(existingUser) {
+      //Profile ID already exists
+      return done(null, existingUser);
+    }
+    
+    //Profile ID not found, create new User Record
+    const user = await new User({ googleId: profile.id }).save();
+    done(null, user);    
   }
 ));
